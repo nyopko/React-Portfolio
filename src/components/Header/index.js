55a6fdc37b163021f3bcc87fcf576a5af039cb23
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-05-15 15:19:31 -0400

    Added header component and sample text, tested and working

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+  return (
+    <div className="main">
+    <div className="row">
+        <div className="col s12 m9">
+            <div className="row">
+                <div className="col s12">
+                    <div className="card-panel">
+                        <h2 className="">Header</h2>
+                    </div>
+                </div>
+            </div>
+           </div>
+          </div>
+         </div>   
+    );
+}
+
+export default Header;
