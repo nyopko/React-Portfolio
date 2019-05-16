a2def0cd91c44a50fc0e1f90b89bc1192f820f2c
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-05-16 14:47:14 -0400

    Added black background, expanded page, contained header text and button into div

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import './style.css';
 
 function Header() {
-  return (
-    <div className="main">
-    <div className="row">
-        <div className="col s12 m9">
+    return (
+        <div className="main">
             <div className="row">
-                <div className="col s12">
-                    <div className="card-panel">
-                        <h2 className="">Header</h2>
+                <div className="col s12 m9">
+                    <div className="row">
+                        <div className="col s12">
+                            <div className="card-panel">
+                                <div className="introText">
+                                    <h2 className="headerText">Nick Yopko <br /> Web Developer, Editor, Digital Marketer</h2>
+                                    <button id="introButton" type="button" className="btn btn-light">See my work!</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-           </div>
-          </div>
-         </div>   
+        </div>
     );
 }
 
